055fda920e206eaa1618baf9797edec1cfe1323b
rohitKnauf <83820553+rohitKnauf@users.noreply.github.com>
2022-01-03 00:50:18 +0100

    add test for addition and subtraction

---
+++ cypress/integration/addition.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe("perform calculation", function () {
+
+  before(function () {
+    cy.fixture('testData.json').then(function (testdata) {
+      this.testdata = testdata
+      cy.visit(this.testdata.url)
+    })
+  })
+
+
+  it("should add two numbers(9+9)", function () {
+    cy.addNumbers()
+  })
+
+  it("should subtract two numbers(9-9)", function () {
+    cy.subtractTwoNumbers()
+  })
+
+  it("should multiply two numbers(9-9)", function () {
+    cy.subtractTwoNumbers()
+  })
+
+
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,49 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addNumbers', () => {
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(4) > .orange > button').should('be.visible').click()
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(5) > .orange > button').should('be.visible').click()
+  expect(cy.get('.component-display > div').contains(`18`))
+})
+
+Cypress.Commands.add('subtractTwoNumbers', () => {
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(3) > .orange > button').should('be.visible').click()
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(5) > .orange > button').should('be.visible').click()
+  cy.get('.component-display > div').contains(`0`)
+})
+
+Cypress.Commands.add('multiplyTwoNumbers', () => {
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(3) > .orange > button').should('be.visible').click()
+  cy.get(':nth-child(2) > :nth-child(3) > button').should('be.visible').click()
+  cy.get(':nth-child(5) > .orange > button').should('be.visible').click()
+  cy.get('.component-display > div').contains(`0`)
+})
